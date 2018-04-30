4a790668bf7b396de0e1893aeaec59bcff8d3991
FlorianGrimm <grimmuko@googlemail.com>
2018-05-01 01:44:55 +0200

    step StringTemplate

---
+++ OfaSchlupfer.Elementary/StringTemplate/Utility.cs
@@ -0,0 +1,27 @@
+﻿namespace OfaSchlupfer.StringTemplate {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using global::Antlr4.StringTemplate;
+
+    using global::Antlr4.StringTemplate.Misc;
+
+    public class Utility {
+        public string Gna() {
+            var t = new Template("a <a.value: {x | [<x>]}; separator=\", \">", '<', '>');
+            //t.Add("a", "1");
+            //t.Add("a", "2");
+            t.Add("a", new { name="n", value=new List<string>() { "eins", "zwei"} });
+            var act = t.Render();
+            //var group = new TemplateGroup();
+            //group.DefineTemplate("name", "template <a>");
+
+            //Template st = group.GetInstanceOf("name");
+
+            //st.Add("a", "b");
+            //var act = st.Render(System.Globalization.CultureInfo.GetCultureInfo(9));
+            return act;
+        }
+    }
+}
---
+++ OfaSchlupfer.ElementaryTests/StringTemplate/UtiltiyTest.cs
@@ -0,0 +1,14 @@
+﻿namespace OfaSchlupfer.StringTemplate {
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+    using Xunit;
+
+    public class UtiltiyTest {
+        [Fact]
+        public void GnaTest() {
+            Assert.Equal("a b", (new Utility()).Gna());
+        }
+    }
+}
